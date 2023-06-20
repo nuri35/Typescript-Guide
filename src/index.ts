2f04bf7e44e1793744a45dafca2fa5b341d247cf
nuri35 <nurie487@gmail.com>
2023-06-20 15:00:51 +0300

    up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import axios from 'axios';
-
+import axios, { Axios, AxiosResponse } from 'axios';
+// Todo: 1. Fix the error by adding an interface to the response.data
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((response) => {
-  console.log(response.data);
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response: AxiosResponse<Todo, any>) => {
+  let datams = response.data; // this is the correct approach
+  // let datams = response.data as Todo; this is the correct approach
+
+  // const todo: Todo = response.data; // ı thınk that wrong approach
+
+  const id: number = datams.id;
+  const title: string = datams.title;
+  const completed: boolean = datams.completed;
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+};
