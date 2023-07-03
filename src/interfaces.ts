4f99ffc2c3946dcedd82d1152ebb1d96dd8c07fb
nuri35 <nurie487@gmail.com>
2023-07-03 11:40:39 +0300

    Up

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,19 +1,31 @@
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  // name: string;
+  // year: number;
+  // broken: boolean; // this delete them
+  summary(): string;
 } // this is new type
 
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 
-printVehicle(oldCivic);
+const printSummary = ({ summary }: Reportable): void => {
+  console.log(summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
